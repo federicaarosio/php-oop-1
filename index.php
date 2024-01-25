3d8da588d3a7391c86ca48fedb0684a9a55a40ac
federicaarosio <arosio.fede@gmail.com>
2024-01-25 16:12:50 +0100

    printed movies in index.php, added style with bootstrap

--- index.php
+++ index.php
@@ -23,13 +23,34 @@ $moviesList = [$movie1, $movie2, $movie3, $movie4];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OOP 1</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
 </head>
 
-<body>
+<body class="bg-light">
     <main>
-        <?php foreach ($moviesList as $movie) {
-            echo $movie->title . "<br>";
-        } ?>
+        <h1 class="text-center m-3">Movies</h1>
+        <div class="container">
+            <div class="row">
+                <?php foreach ($moviesList as $movie) { ?>
+
+                <div class="card m-3" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $movie->title ?></h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">A movie by: <?php echo $movie->director?>
+                        </h6>
+                        <p class="card-text"><?php echo $movie->synopsis ?></p>
+                        <p class="card-text">Genre: <?php echo $movie->genre ?></p>
+                    </div>
+                </div>
+
+                <?php } ?>
+
+            </div>
+        </div>
     </main>
 </body>
 
